4fc08e9183dd1a423b24e22e642b4550307668ba
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-04-04 20:31:58 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        1)***
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(" Enter 5 words :");
+//        String str0 = input.nextLine();
+//        String str1 = input.nextLine();
+//        String str2 = input.nextLine();
+//        String str3 = input.nextLine();
+//        String str4 = input.nextLine();
+//        System.out.println(str0.concat(" ").concat(str1.concat(" ").concat(str2.concat(" ").concat(str3.concat(" ").concat(str4)))));
+
+//        ***
+
+//        2)***
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(" Enter 5 words :");
+//        String str0 = input.nextLine();
+//        String str1 = input.nextLine();
+//        String str2 = input.nextLine();
+//        String str3 = input.nextLine();
+//        String str4 = input.nextLine();
+//        String pos0,pos1,pos2,pos3,pos4;
+//        pos0 = str0.substring(0,1);
+//        pos1=str1.substring(0, 1);
+//        pos2=str2.substring(0,1);
+//        pos3=str3.substring(0,1);
+//        pos4=str4.substring(0,1);
+//        System.out.println(pos0+" "+pos1+" "+pos2+" "+pos3+" "+pos4);
+
+//        ***
+
+//        3)***
+
+//        double a ,b, c, max;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Введіть дробове число : ");
+//        if (input.hasNextDouble()){
+//        a = input.nextDouble();
+//            System.out.print("Введіть дробове число : ");
+//            if (input.hasNextDouble()){
+//                b = input.nextDouble();
+//                System.out.print("Введіть дробове число : ");
+//                if (input.hasNextDouble()){
+//                    c= input.nextDouble();
+//                    if ((a == b) && (b == c)){
+//                        System.out.println(" однакові9 ");
+//                    } else if ((a>b)&&(a>c)) {
+//                        System.out.println("max = "+a);
+//                    } else if ((b>a)&&(b>c)) {
+//                        System.out.println("max = "+b);
+//                    }else{System.out.println("max = "+c);}
+//                }else {
+//                    System.out.println("не дробове");
+//                }
+//
+//            }else {
+//                System.out.println("не дробове");
+//            }
+//
+//        }else {
+//            System.out.println("не дробове");
+//        }
+
+//        ***
+//        4)***
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Визначимо чи ти вмієш писати однакові стрічки");
+//        System.out.print("стрічка номер 1 :");
+//        String name = input.nextLine();
+//        System.out.print("стрічка номет 2 :");
+//        String name2 = input.nextLine();
+//        if (name.equalsIgnoreCase(name2)){
+//            System.out.println(name +" = "+name2);
+//        }else {
+//            System.out.println("Fuck your mama ");
+//        }
+
+//        ***
+//        5)***
+//        Scanner input = new Scanner(System.in);
+//        System.out.print(" перша стрічка : ");
+//        String str1 = input.nextLine();
+//        System.out.print(" друга стрічка : ");
+//        String str2 = input.nextLine();
+//        int a,b;
+//        a = str1.length();
+//        b = str2.length();
+//        if (a>b){
+//            System.out.println("max  str_1 = "+a+"; min str_2 = "+b+";");
+//        }else {
+//            System.out.println("max str_2 = "+b+"; min str_1 = "+a+";");}
+
+//        ***
+
+    }
+}
